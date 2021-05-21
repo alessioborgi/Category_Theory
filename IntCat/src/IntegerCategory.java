8ed2fbcf6eebdab7dbbe67af7f00d20d066921b0
Francesco Danese <76563591+Paco-Danes@users.noreply.github.com>
2021-05-21 07:09:50 +0200

    Update IntegerCategory.java

--- IntCat/src/IntegerCategory.java
+++ IntCat/src/IntegerCategory.java
@@ -1,5 +1,5 @@
 import java.util.concurrent.ThreadLocalRandom;
-//import java.lang.Math;
+import java.lang.Math;
 
 public class IntegerCategory{
 
@@ -52,7 +52,7 @@ public class IntegerCategory{
                     return false;
                 }
             }
-        return true;}
+            return true;}
     }
 
     static class newGroup implements Group<Integer>{
@@ -91,49 +91,93 @@ public class IntegerCategory{
                 Integer randomZ = ThreadLocalRandom.current().nextInt(0,100);
                 if (! isAbelian(randomN,randomM,randomZ)){return false;}
             }
-        return true;}
+            return true;}
     }
 
-    static class newRing implements Ring<Integer>{}
+    static class newRing implements Ring<Integer>{
 
-    /*private class IntSum implements Group<Integer> {
-        @Override
-        public Integer apply(Integer t, Integer u) { return t+u; }
+        int zeroAdd;
+        int zeroMul;
+        String invA;
+        String fxAdd;
+        String fxMult;
+        newGroup ringGroup;
+        newMonoid ringMonoid;
 
-        public Integer id() { return 0; }
-
-        public  Integer invert(Integer t) { return -t; }
-    }*/
-
-    /*private class IntMul implements Monoid<Integer>{
-        @Override
-        public Integer apply(Integer t, Integer u) {
-            return t*u;
+        public newRing(int eA,int eM,String addition,String multiplication,String inversion){
+            zeroAdd = eA;
+            zeroMul = eM;
+            invA = inversion;
+            fxAdd = addition;
+            fxMult = multiplication;
+            ringGroup = new newGroup(zeroAdd,fxAdd,invA);
+            ringMonoid = new newMonoid(zeroMul,fxMult);
         }
 
-        public Integer id() {
-            return 1;
-        }
-    }*/
-
-    /*private class BoolAnd implements Monoid<Boolean>{
         @Override
-        public Boolean apply(Boolean t, Boolean u) { return t && u;}
+        public Integer idAdd() {
+            return ringGroup.id();
+        }
 
-        public Boolean id(){ return true;}
-        }*/
+        @Override
+        public Integer idMult() {
+            return ringMonoid.id();
+        }
 
-    //testing things
+        @Override
+        public Integer invAdd(Integer t) {
+            return ringGroup.invert(t);
+        }
+
+        @Override
+        public Integer add(Integer t, Integer u) {
+            return ringGroup.apply(t,u);
+        }
+
+        @Override
+        public Integer mult(Integer t, Integer u) {
+            return ringMonoid.apply(t,u);
+        }
+
+        private boolean distributivityTest(){
+            for(int i = 0; i < 50; i++){
+                Integer randomN = ThreadLocalRandom.current().nextInt(-100,100);
+                Integer randomM = ThreadLocalRandom.current().nextInt(-100,0);
+                Integer randomZ = ThreadLocalRandom.current().nextInt(0,100);
+                Integer randomK = ThreadLocalRandom.current().nextInt(-100,0);
+                try {
+                    distributivityTest(randomN, randomM, randomZ);
+                    distributivityTest(randomN, randomZ, randomM);
+                    distributivityTest(randomM, randomN, randomZ);
+                    distributivityTest(randomM, randomK, randomZ);
+                    distributivityTest(randomK, randomN, randomZ);
+                }
+                catch (Exception e){
+                    System.out.println("Exception caught => " + e.getMessage());
+                    return false;
+                }
+            }
+        return true;}
+
+        public boolean test(){
+            return ringGroup.isAbelian() && ringMonoid.test() && distributivityTest();
+        }
+
+    }
 
     public void met(){
         try {
-            newGroup add = new newGroup(0,"+","-");
-            newMonoid mult = new newMonoid(1,"*");
+            newGroup add = new newGroup(0, "+", "-");
+            newMonoid mult = new newMonoid(1, "/");
             newSemigroup sub = new newSemigroup("-");
+            newRing intRing = new newRing(0, 1, "+", "*", "-");
             System.out.println(add.isAbelian());
-            if (mult.test() && add.test()){
-                System.out.println(sub.apply(8,9));
-                System.out.println(mult.apply(3,4));}
+            if (mult.test() && add.test() && intRing.test()) {
+                System.out.println(sub.apply(8, 9)); //-1
+                System.out.println(mult.apply(8, 4)); //12
+                System.out.println(intRing.add(7, 3)); //10 or 4
+                System.out.println(intRing.mult(8, 4)); //-20
+            }
         }
         catch (Exception e){
             System.out.println("Exception caught => " + e.getMessage());
