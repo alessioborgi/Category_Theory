f8bfc216c397bd9533f070c6ef9e67269f483bf7
Francesco Danese <76563591+Paco-Danes@users.noreply.github.com>
2021-05-21 07:10:03 +0200

    Update Ring.java

--- IntCat/src/Ring.java
+++ IntCat/src/Ring.java
@@ -1,2 +1,18 @@
+
 public interface Ring<T> {
+
+    T add(T t,T u);
+    T mult(T t,T u);
+    T idAdd();
+    T invAdd(T t);
+    T idMult();
+
+    default void distributivityTest(T t,T u, T r) throws Exception{
+        if (mult(t,add(u,r)).equals(add(mult(t,u),mult(t,r)))){
+            //System.out.println("Correct distribution);
+        }
+        else {
+            throw new Exception("Error: Distribution is not satisfied");
+        }
+    }
 }
