2ba819c17db2061a6b1cfdd03bb68172fa1505d0
elenamuia <82598315+elenamuia@users.noreply.github.com>
2021-05-27 12:33:25 +0200

    Update New_Monoid.java

--- Login/stages/New_Monoid.java
+++ Login/stages/New_Monoid.java
@@ -1,11 +1,5 @@
 package Login.stages;
 
-/**
- * @author alessioborgi
- * @created 24 / 05 / 2021 - 15:53
- * @project CATEGORY_THEORY
- */
-
 import Integer_Category.IntegerCategory;
 import Login.JavaFX;
 import Set_Category.SetCat;
@@ -135,9 +129,9 @@ public class New_Monoid extends Application {
 
 
         ComboBox comboBox = new ComboBox();
-        comboBox.getStyleClass().add("menu-create");
         comboBox.setPrefHeight(30);
         comboBox.setMinWidth(30);
+        comboBox.getStyleClass().add("menu-create");
         ObservableList<String> options =
                 FXCollections.observableArrayList(
                         "+",
@@ -156,6 +150,7 @@ public class New_Monoid extends Application {
                         "¬V",
                         "¬⊕"
                 );
+
 
 
         integer.setOnAction(new EventHandler<ActionEvent>() {
@@ -191,6 +186,11 @@ public class New_Monoid extends Application {
                     fine.setText("                                         WRONG IDENTITY!");
                     fine.setStyle("-fx-font-size: 22 px");
                 }
+            if(type.getText().equals("Boolean")){
+
+
+
+            }
 
             }
 
@@ -199,87 +199,91 @@ public class New_Monoid extends Application {
 
 
         create.setOnAction(e->{
-            test.fire();
+                test.fire();
 
-            if(fine.getText().equals("                                              TEST PASSED!")) {
-                Circle circle = new Circle();
-                circle.setRadius(250);
-                circle.setStroke(Color.BLACK);
-                circle.setFill(JavaFX.background);
-                circle.setCenterX(400.0f);
-                circle.setCenterY(300.0f);
+                if(fine.getText().equals("                                              TEST PASSED!")) {
+                    Circle circle = new Circle();
+                    circle.setRadius(250);
+                    circle.setStroke(Color.BLACK);
+                    circle.setFill(JavaFX.background);
+                    circle.setCenterX(400.0f);
+                    circle.setCenterY(300.0f);
 
-                Label name = new Label("NEW MONOID");
-                name.setStyle("-fx-font-size: 25 px");
-                name.setTranslateX(250.0f);
-                name.setTranslateY(150.0f);
+                    Label name = new Label("NEW MONOID");
+                    name.setStyle("-fx-font-size: 25 px");
+                    name.setTranslateX(250.0f);
+                    name.setTranslateY(150.0f);
 
-                TextField a = new TextField();
-                a.setPromptText("a...");
-                a.setMinHeight(30);
-                a.setTranslateX(175.0f);
-                a.setTranslateY(300.0f);
+                    TextField a = new TextField();
+                    a.setPromptText("a...");
+                    a.setMinHeight(30);
+                    a.setTranslateX(175.0f);
+                    a.setTranslateY(300.0f);
 
-                TextField b = new TextField();
-                b.setPromptText("b...");
-                b.setMinHeight(30);
-                b.setTranslateX(475.0f);
-                b.setTranslateY(300.0f);
-
-
-                Label leftArrow = new Label("->");
-                leftArrow.setStyle("-fx-font-size: 22 px");
-                leftArrow.setTranslateY(300.0f);
-                leftArrow.setTranslateX(325.0f);
-
-                Label rightArrow = new Label("<-");
-                rightArrow.setStyle("-fx-font-size: 22 px");
-                rightArrow.setTranslateX(450.0f);
-                rightArrow.setTranslateY(300.0f);
-
-                Label result = new Label();
-                result.setTranslateY(300.0f);
-                result.setTranslateX(380.0f);
+                    TextField b = new TextField();
+                    b.setPromptText("b...");
+                    b.setMinHeight(30);
+                    b.setTranslateX(475.0f);
+                    b.setTranslateY(300.0f);
 
 
-                Button func = new Button("f");
-                func.setStyle("-fx-font-size: 25 px");
-                func.setTranslateX(380.0f);
-                func.setTranslateY(375.0f);
-                func.getStyleClass().add("button-create");
+                    Label leftArrow = new Label("->");
+                    leftArrow.setStyle("-fx-font-size: 22 px");
+                    leftArrow.setTranslateY(300.0f);
+                    leftArrow.setTranslateX(325.0f);
 
-                String x = "Type: "+ type.getText() +"\n" +
-                        "Operation: " + comboBox.getValue() +"\n"+
-                        "Identity: " + inputText.getText()
-                        ;
-                Tooltip t = new Tooltip(x);
-                Button info = new Button("i");
-                info.setShape(new Circle(1.5));
-                info.setTranslateX(500.0f);
-                info.setTranslateY(125.0f);
-                info.setStyle("-fx-font-size: 30 px");
-                info.getStyleClass().add("button-create");
+                    Label rightArrow = new Label("<-");
+                    rightArrow.setStyle("-fx-font-size: 22 px");
+                    rightArrow.setTranslateX(450.0f);
+                    rightArrow.setTranslateY(300.0f);
 
-                info.setTooltip(t);
+                    Label result = new Label();
+                    result.setTranslateY(300.0f);
+                    result.setTranslateX(380.0f);
 
 
+                    Button func = new Button("f");
+                    func.setStyle("-fx-font-size: 25 px");
+                    func.setTranslateX(380.0f);
+                    func.setTranslateY(375.0f);
+                    func.getStyleClass().add("button-create");
 
-                info.setOnAction(f->{
-                    Alert alertInfo = new Alert(Alert.AlertType.INFORMATION);
-                    alertInfo.setTitle("Info");
-                    alertInfo.setHeaderText("Monoid Features");
-                    String s = "Type: "+ type.getText() +"\n" +
+
+                    String x = "Type: "+ type.getText() +"\n" +
                             "Operation: " + comboBox.getValue() +"\n"+
                             "Identity: " + inputText.getText()
                             ;
-                    alertInfo.setContentText(s);
-                    alertInfo.show();
+                    Tooltip t = new Tooltip(x);
+                    Button info = new Button("i");
+                    info.setShape(new Circle(1.5));
+                    info.setTranslateX(500.0f);
+                    info.setTranslateY(125.0f);
+                    info.setStyle("-fx-font-size: 30 px");
+                    info.getStyleClass().add("button-create");
 
-                });
+                    info.setTooltip(t);
 
-                JavaFX.draw.getChildren().clear();
-                JavaFX.draw.getChildren().addAll(circle, name, a, leftArrow, b, rightArrow, func, result, info);
-            }
+
+
+                    info.setOnAction(f->{
+                        Alert alertInfo = new Alert(Alert.AlertType.INFORMATION);
+                        alertInfo.setTitle("Info");
+                        alertInfo.setHeaderText("Monoid Features");
+                        String s = "Type: "+ type.getText() +"\n" +
+                                "Operation: " + comboBox.getValue() +"\n"+
+                                "Identity: " + inputText.getText()
+                                ;
+                        alertInfo.setContentText(s);
+                        alertInfo.show();
+
+                    });
+
+
+                    JavaFX.draw.getChildren().clear();
+                    JavaFX.draw.getChildren().addAll(circle, name, a, leftArrow, b, rightArrow, func, result, info);
+
+
+                }
         });
 
 
@@ -291,4 +295,4 @@ public class New_Monoid extends Application {
         monoidVBox.getChildren().addAll(typeHBox, morphHBox, identityHBox, checkHBox, endHBox);
         monoid.show();
     }
-}
+    }
