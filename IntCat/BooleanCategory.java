53f4ebd09cbfad6fdb2c75ea1f43bdb4ab34a925
Francesco Danese <76563591+Paco-Danes@users.noreply.github.com>
2021-05-27 17:52:42 +0200

    Update BooleanCategory.java

--- IntCat/BooleanCategory.java
+++ IntCat/BooleanCategory.java
@@ -1,7 +1,9 @@
+package Integer_Category;
+import java.util.ArrayList;
 
 public class BooleanCategory {
 
-    static class newSemigroup implements Semigroup<Boolean> {
+    public static class newSemigroup implements Semigroup<Boolean> {
         String fx;
 
         public newSemigroup(String func) {
@@ -10,9 +12,9 @@ public class BooleanCategory {
 
         public Boolean apply(Boolean t, Boolean u) {
             return switch (fx) {
-                case "and" -> t && u;
-                case "or" -> t || u;
-                case "xor" -> t ^ u;
+                case "∧" -> t && u;
+                case  "V" -> t || u;
+                case "⊕" -> t ^ u;
                 case "nand" -> !(t && u);   //not associative
                 case "nor" -> !(t || u);      //not associative
                 case "xnor" -> !(t ^ u);    //not associative
@@ -22,7 +24,7 @@ public class BooleanCategory {
         }
     }
 
-    static class newMonoid implements Monoid<Boolean> {
+    public static class newMonoid implements Monoid<Boolean> {
 
         boolean zero;
         String fx;
@@ -48,6 +50,71 @@ public class BooleanCategory {
                 System.out.println("Exception caught => " + e.getMessage());
                 return false;}
         return true;}
+    }
+
+    public static class newGroup implements Group<Boolean> {
+
+        boolean zero;
+        String fx;
+        String inv;
+
+        public newGroup(boolean e, String func, String invFunc) {
+            zero = e;
+            fx = func;
+            inv = invFunc;
+        }
+
+        public Boolean apply(Boolean t, Boolean u) {
+            return new BooleanCategory.newSemigroup(fx).apply(t, u);
+        }
+
+        public Boolean id() {
+            return zero;
+        }
+
+        public Boolean invert(Boolean t) {
+            return switch (inv) {
+                case "+a" -> t;
+                case "-a" -> !t;
+                case "0" -> false;
+                default -> true;
+            };
+        }
+
+        public ArrayList test() {
+            ArrayList AA = new ArrayList();
+            try{test(true);
+                test(false);}
+            catch (Exception e) {System.out.println("Exception caught => " + e.getMessage());
+                AA.add(false);
+                AA.add(e.getMessage());
+                return AA;}
+            AA.add(true);
+            AA.add("TEST PASSED!");
+            return AA;}
+
+        public boolean isAbelian() {
+            boolean flag;
+            Character c;
+            String bits;
+            for (int i = 0; i < 8; i++) {
+                ArrayList<Boolean> bbits = new ArrayList<Boolean>();
+                bits = String.format("%3s", Integer.toBinaryString(i)).replace(' ', '0');
+                for (int j = 0; j < 2; j++) {
+                    c = bits.charAt(j);
+                    if (c.equals('0')) {
+                        bbits.add(false);
+                    } else {
+                        bbits.add(true);
+                    }
+                }
+                flag = isAbelian(bbits.get(0), bbits.get(1), bbits.get(0));
+                if (!flag) {
+                    return false;
+                }
+            }
+            return true;
+        }
     }
 
 
