60bda41643669cb237d43e14da18ca2789a2e6e2
elenamuia <82598315+elenamuia@users.noreply.github.com>
2021-05-28 23:01:09 +0200

    Update Controller.java

--- Free_Drawing/Controller.java
+++ Free_Drawing/Controller.java
@@ -2,13 +2,14 @@ package Free_Drawing;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.Label;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import Free_Drawing.menu.New_Vertex;
 
@@ -20,6 +21,7 @@ import java.util.Optional;
  * @project CATEGORY_THEORY
  */
 public class Controller {
+
     @FXML
     public AnchorPane graph;
     public Vertex vertex1;
@@ -27,6 +29,7 @@ public class Controller {
     public Vertex vertexDelete;
     public Arrow arrow;
     public Button button;
+    public static TextField idNode1Text;
     public Label label1;
 
 
@@ -176,8 +179,68 @@ public class Controller {
     }
 
     public void NewVertexClicked(ActionEvent actionEvent) {
-        Stage group = new Stage();
-        new New_Vertex().start(group);
+        Stage new_vertex = new Stage();
+        new_vertex.setWidth(700);
+        new_vertex.setHeight(500);
+        new_vertex.setTitle("New Vertex");
+
+        VBox newVertexVBox = new VBox();
+        newVertexVBox.getStyleClass().add("background");
+        Scene newVertexScene = new Scene(newVertexVBox);
+        newVertexScene.getStylesheets().add("Login/Styles.css");
+
+        new_vertex.setScene(newVertexScene);
+
+        //////////////////////////////////
+
+        HBox idNode1HBox = new HBox();
+        idNode1HBox.setMinHeight(100);
+        idNode1HBox.setMinWidth(700);
+        idNode1HBox.setAlignment(Pos.CENTER_LEFT);
+        HBox idNode2HBox = new HBox();
+        idNode2HBox.setMinHeight(100);
+        idNode2HBox.setMinWidth(700);
+        idNode2HBox.setAlignment(Pos.CENTER_LEFT);
+        HBox compositionHBOX = new HBox();
+        compositionHBOX.setMinHeight(100);
+        compositionHBOX.setMinWidth(700);
+        compositionHBOX.setAlignment(Pos.CENTER_LEFT);
+        HBox createHBOX = new HBox();
+        createHBOX.setMinHeight(100);
+        createHBOX.setMinWidth(700);
+        createHBOX.setAlignment(Pos.CENTER);
+
+
+        Label idNode1 = new Label("    ID Node 1:   ");
+        idNode1.setStyle("-fx-font-size: 30 px");
+        Label idNode2 = new Label("    ID Node 2:  ");
+        idNode2.setStyle("-fx-font-size: 30 px");
+        Label composition = new Label("   Composition:    ");
+        composition.setStyle("-fx-font-size: 30 px");
+
+        idNode1Text = new TextField();
+        idNode1Text.setPromptText("Insert the ID...");
+        idNode1Text.setMinHeight(25);
+
+        TextField idNode2Text = new TextField();
+        idNode2Text.setPromptText("Insert the ID...");
+        idNode2Text.setMinHeight(25);
+
+        TextField compositionText = new TextField();
+        compositionText.setPromptText(" ID node...");
+        compositionText.setMinHeight(25);
+
+        Button create = new Button("Create");
+        create.setStyle("-fx-font-size: 22 px");
+        create.getStyleClass().add("button-create");
+
+        idNode1HBox.getChildren().addAll(idNode1, idNode1Text);
+        idNode2HBox.getChildren().addAll(idNode2, idNode2Text);
+
+        compositionHBOX.getChildren().addAll(composition, compositionText);
+        createHBOX.getChildren().addAll(create);
+        newVertexVBox.getChildren().addAll(idNode1HBox, idNode2HBox, compositionHBOX, createHBOX);
+        new_vertex.show();
     }
 
     public void NewComposition(ActionEvent actionEvent) {
