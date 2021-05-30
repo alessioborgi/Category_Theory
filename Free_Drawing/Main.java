e018604b8d47b46fab24a4558b9a65079f2ce205
Alessio_Borgi <73711270+alessioborgi@users.noreply.github.com>
2021-05-30 21:24:12 +0200

    Update Main.java

--- Free_Drawing/Main.java
+++ Free_Drawing/Main.java
@@ -1,4 +1,6 @@
-package Free_Drawing; /**
+package Free_Drawing;
+
+/**
  * @created 22 / 05 / 2021 - 14:40
  * @project CATEGORY_THEORY
  * @author alessioborgi
@@ -8,20 +10,24 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    /*
+        This class has the power to create the stage for the new Free_Drawing stage.
+     */
     public static Parent root;
-
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        /*
+            Implementation of the Abstract Class of the Application class.
+         */
         root = FXMLLoader.load(getClass().getResource("Free.fxml"));
-        Scene  scene  =  new  Scene(root, 800, 800);
+        Scene  scene  =  new  Scene(root, 700, 775);
         scene.getStylesheets().add("MyStyles.css");
         primaryStage.setTitle("Free Graph");
         primaryStage.setScene(scene);
