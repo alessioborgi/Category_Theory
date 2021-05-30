3d391f5296c52ca2e324a9ece347428d73f0c0b1
Alessio_Borgi <73711270+alessioborgi@users.noreply.github.com>
2021-05-30 15:24:23 +0200

    Update New_Login.java

--- Login/New_Login.java
+++ Login/New_Login.java
@@ -1,31 +1,31 @@
 package Login;
 
-import Login.Menu;
-
-import javax.swing.*;
-import java.awt.*;
-
 /**
  * @author alessioborgi
  * @created 03 / 05 / 2021 - 17:09
  * @project LOGIN
  */
+
+import java.awt.*;
+
 public class New_Login {
+    /*
+        Class the contains the Main for starting the application. It will runs the
+        Login_Start JFrame window, the one with the Login implementation.
+     */
 
     public static void main(String[] args) {
         //SwingUtilities.invokeLater(() -> new Login_Start());
-        ///*
         EventQueue.invokeLater(new Runnable() {
             @Override
             public void run() {
                 try{
-                    //Login_Start frame_1 = new Login_Start();
-                    Login.Menu frame_1 = new Menu();
+                    Login_Start frame_1 = new Login_Start();
+                    //Login.Menu frame_1 = new Menu();
                 }catch(Exception e){
                     e.printStackTrace();
                 }
             }
         });
-    //*/
     }
 }
