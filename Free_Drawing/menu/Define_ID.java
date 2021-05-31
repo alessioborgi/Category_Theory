d8af89f9f2da83a707ac8b5f2b25d3f4a23ad521
elenamuia <82598315+elenamuia@users.noreply.github.com>
2021-05-31 11:45:33 +0200

    Update Define_ID.java

--- Free_Drawing/menu/Define_ID.java
+++ Free_Drawing/menu/Define_ID.java
@@ -1,11 +1,4 @@
 package Free_Drawing.menu;
-/**
- * @created 25 / 05 / 2021 - 23:13
- * @project CATEGORY_THEORY
- * @author alessioborgi
- */
-import Free_Drawing.Controller;
-import Free_Drawing.Vertex;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.control.*;
@@ -13,13 +6,23 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-import java.util.Random;
+/**
+ * @author elenamuia
+ * @author alessioborgi
+ * @author federicavaleau
+ * @author francescodanese
+ * @created 24 / 05 / 2021 - 15:52
+ * @project CATEGORY_THEORY
+ */
+
+/**
+ * This code opens a window from the button "Free Drawing" in the Draw menu from the main page.
+ * In this window, the user can define the Id of the created vertex. So the user can add the ID in the proper TextField.
+ * Then when "Create" is pressed, in the last page a new vertex is created having the selected ID and if the user 
+ * doesn't set a new ID, the following one will be again the increasing numbers (look at Vertex.java)
+ */
 
 public class Define_ID extends Application{
 
@@ -27,7 +30,6 @@ public class Define_ID extends Application{
     public static Button create;
 
     public void start(Stage new_vertex) {
-
 
         new_vertex.setWidth(500);
         new_vertex.setHeight(250);
@@ -37,10 +39,7 @@ public class Define_ID extends Application{
         newVertexVBox.getStyleClass().add("background");
         Scene newVertexScene = new Scene(newVertexVBox);
         newVertexScene.getStylesheets().add("Login/Styles.css");
-
         new_vertex.setScene(newVertexScene);
-
-        //////////////////////////////////
 
         HBox idNode1HBox = new HBox();
         idNode1HBox.setMinHeight(100);
@@ -51,7 +50,6 @@ public class Define_ID extends Application{
         createHBOX.setMinHeight(100);
         createHBOX.setMinWidth(500);
         createHBOX.setAlignment(Pos.CENTER);
-
 
         Label idNode1 = new Label("    ID Node 1:   ");
         idNode1.setStyle("-fx-font-size: 30 px");
