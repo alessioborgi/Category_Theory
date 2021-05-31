ad9c9164e06fd3b250e1028fa3d98eacf67db148
Francesco Danese <76563591+Paco-Danes@users.noreply.github.com>
2021-05-31 11:12:17 +0200

    Update Ring.java

--- IntCat/Ring.java
+++ IntCat/Ring.java
@@ -1,4 +1,6 @@
+package Integer_Category;
 
+//a basic interface for rings
 public interface Ring<T> {
 
     T add(T t,T u);
@@ -7,6 +9,7 @@ public interface Ring<T> {
     T invAdd(T t);
     T idMult();
 
+    //test if distribution is satisfied
     default void distributivityTest(T t,T u, T r) throws Exception{
         if (mult(t,add(u,r)).equals(add(mult(t,u),mult(t,r)))){
             //System.out.println("Correct distribution);
